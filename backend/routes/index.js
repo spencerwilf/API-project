05b0fe6d3d5583e311c97c059d8195434ec25f3a
spencerwilf <spencerwilf@yahoo.com>
2023-02-15 13:18:31 -0800

    set api routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 router.get('/api/csrf/restore', (req, res) => {
     const csrfToken = req.csrfToken();
@@ -9,4 +10,6 @@ router.get('/api/csrf/restore', (req, res) => {
     });
 });
 
-  module.exports = router;
+router.use('/api', apiRouter);
+
+module.exports = router;
