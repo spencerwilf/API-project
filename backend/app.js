8cf5e9e06c7fdd95534623fea98fdb980457230a
spencerwilf <spencerwilf@yahoo.com>
2023-02-16 10:59:05 -0800

    updated error handling

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.use(
   app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
     err.title = "Resource Not Found";
-    err.errors = ["The requested resource couldn't be found."];
+    err.errors = { message: "The requested resource couldn't be found." };
     err.status = 404;
     next(err);
   });
@@ -49,8 +49,12 @@ app.use(
   app.use((err, _req, _res, next) => {
     // check if error is a Sequelize error:
     if (err instanceof ValidationError) {
-      err.errors = err.errors.map((e) => e.message);
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
       err.title = 'Validation error';
+      err.errors = errors;
     }
     next(err);
   });
